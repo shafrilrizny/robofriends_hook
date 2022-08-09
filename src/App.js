963747be5d802e6404838cabf190e2b953700ef6
shafrilrizny <riznyapp@gmail.com>
2022-08-09 19:51:58 +0800

    updated files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import Cardlist from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import './App.css';
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>RoboFriends</h1>
-            < SearchBox />
-            < Cardlist robots = { robots } />
-        </div>
-    )
+class App extends Component {
+    constructor () {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(user => {this.setState({robots: user})});
 }
+
+    onChangeSearch = (searched) => {
+        this.setState({ searchfield: searched.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        }) 
+
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                < SearchBox searchChange = { this.onChangeSearch }/>
+                < Cardlist robots = { filteredRobots } />
+            </div>
+        )
+    }
+}
+
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({ id, name, email}) => {
     return(
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='bg-card dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img src={ `https://robohash.org/${id}?200x200` } alt='robot'/>
             <div>
                 <h2>{ name }</h2>
