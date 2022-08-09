ed29ec4d3856a400fb3d03f06fa1d1d12e995092
shafrilrizny <riznyapp@gmail.com>
2022-08-09 18:16:52 +0800

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Cardlist from "./CardList";
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learning React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="tc">
+            <h1>RoboFriends</h1>
+            < SearchBox />
+            < Cardlist robots = { robots } />
+        </div>
+    )
 }
 
 export default App;
